70e3320e26fed39f02f3770c0edd2733b32c73d7
Jana Schmehrsahl <j.schmehrsahl@icloud.com>
2022-12-13 08:31:23 +0100

    Initial Commit

---
+++ Starwars SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Starwars SwiftUI
+//
+//  Created by Jana Schmehrsahl on 13.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Starwars SwiftUI/Starwars_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starwars_SwiftUIApp.swift
+//  Starwars SwiftUI
+//
+//  Created by Jana Schmehrsahl on 13.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Starwars_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
